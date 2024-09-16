629488c7f44abc92503944ba76295574c2a1ccdf
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2024-09-16 23:15:10 +0100

    feat: plot rutime and compile time results

--- results-to-csv.py
+++ results-to-csv.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import os
 import xml.etree.ElementTree as ET
@@ -122,6 +123,44 @@ def merge_object_size_results(results_file):
     pivot_table.to_csv(results_file, sep=";")
 
 
+def plot_compile_time_results(results_file, plot_dir):
+    df = pd.read_csv(results_file, sep=";")
+    tests = df["Test"].unique()
+    for test in tests:
+        plot_file = f"{plot_dir}/{test}.png"
+        print(f"Plotting compile time for {test} in {plot_file}")
+        test_df = df[df["Test"] == test]
+        test_df.plot(x="Profile", y="Compile Time", kind="barh")
+        plt.title(f"Compile Time for {test}")
+        plt.ylabel("Time (ms)")
+        plt.xlabel("Profile")
+        plt.savefig(plot_file)
+        plt.close()
+
+
+# Do a plot for each test and, for each test, do a plot for each description
+# If the test only has one description, just do one plot for that test
+def plot_runtime_results(results_file, plot_dir):
+    df = pd.read_csv(results_file, sep=";")
+    tests = df["Test"].unique()
+    for test in tests:
+        test_df = df[df["Test"] == test]
+        descriptions = test_df["Description"].unique()
+        for description in descriptions:
+            escaped_description = description.replace(" ", "-").replace(":", "").lower()
+            plot_file = f"{plot_dir}/{test.split("/")[-1]}-{escaped_description}.png"
+            print(f"Plotting runtime for {test} in {plot_file}")
+            description_df = test_df[test_df["Description"] == description]
+            description_df.plot(x="Profile", y="Value", kind="barh")
+            scale = description_df["Scale"].iloc[0]
+            proportion = description_df["Proportion"].iloc[0]
+            plt.title(f"Runtime for {test} ({description})")
+            plt.ylabel(f"{scale} ({proportion})")
+            plt.xlabel("Profile")
+            plt.savefig(plot_file)
+            plt.close()
+
+
 if __name__ == "__main__":
 
     # User must supply results directory
@@ -130,6 +169,9 @@ if __name__ == "__main__":
     parser.add_argument("results_dir", type=str, help="Results directory")
     parser.add_argument(
         "-m", "--merge", action="store_true", help="Merge results across profiles"
+    )
+    parser.add_argument(
+        "-p", "--plot", action="store_true", help="Plot results using matplotlib"
     )
     args = parser.parse_args()
 
@@ -155,6 +197,23 @@ if __name__ == "__main__":
     write_object_size_results(
         get_object_size_results(results_dir), OBJECT_SIZE_RESULTS_FILE
     )
+
+    if args.plot:
+        PLOT_PATH = results_dir + "/plots"
+        if not os.path.exists(PLOT_PATH):
+            os.makedirs(PLOT_PATH)
+
+        COMPILE_TIME_PLOT_DIR = PLOT_PATH + "/compile-time"
+        if not os.path.exists(COMPILE_TIME_PLOT_DIR):
+            os.makedirs(COMPILE_TIME_PLOT_DIR)
+        plot_compile_time_results(COMPILE_TIME_RESULTS_FILE, COMPILE_TIME_PLOT_DIR)
+
+        RUNTIME_PLOT_DIR = PLOT_PATH + "/runtime"
+        if not os.path.exists(RUNTIME_PLOT_DIR):
+            os.makedirs(RUNTIME_PLOT_DIR)
+        plot_runtime_results(RUNTIME_RESULTS_FILE, RUNTIME_PLOT_DIR)
+        exit(0)
+        plot_object_size_results(OBJECT_SIZE_RESULTS_FILE)
 
     if args.merge:
         merge_compile_time_results(COMPILE_TIME_RESULTS_FILE)
