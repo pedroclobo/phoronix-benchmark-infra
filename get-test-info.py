4f359d285deb195c703d27f3bebc2cfd29fa9427
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2024-11-11 11:22:46 +0000

    feat(get-test-info): add csv and markdown writers

--- get-test-info.py
+++ get-test-info.py
@@ -1,6 +1,38 @@
+import csv
+import sys
 import os
 import xml.etree.ElementTree as ET
 import argparse
+from tabulate import tabulate
+
+
+class Writer:
+    def __init__(self):
+        pass
+
+    def write(self, test_info):
+        pass
+
+
+class MarkdownWriter(Writer):
+    def write(self, test_info):
+        headers = ["Name", "Version", "Description"]
+        rows = [
+            [info["name"], info["version"], info["description"]] for info in test_info
+        ]
+
+        sys.stdout.write("# Test Profiles\n\n")
+        sys.stdout.write(tabulate(rows, headers, tablefmt="pipe"))
+        sys.stdout.write("\n")
+
+
+class CSVWriter(Writer):
+    def write(self, test_info):
+        writer = csv.DictWriter(
+            sys.stdout, fieldnames=["name", "version", "description"]
+        )
+        writer.writeheader()
+        writer.writerows(test_info)
 
 
 def parse_test_profile(test_name, path):
@@ -24,6 +56,13 @@ if __name__ == "__main__":
     parser.add_argument(
         "test_names_file", type=str, help="Path to a file containing test names"
     )
+    parser.add_argument(
+        "--output-format",
+        type=str,
+        choices=["markdown", "csv"],
+        default="markdown",
+        help="Output format",
+    )
     args = parser.parse_args()
 
     if not os.path.exists(args.test_profiles_dir):
@@ -40,3 +79,9 @@ if __name__ == "__main__":
     for test_name in test_names_file:
         path = os.path.join(args.test_profiles_dir, test_name, "test-definition.xml")
         test_info += [parse_test_profile(test_name, path)]
+
+    if args.output_format == "markdown":
+        writer = MarkdownWriter()
+    elif args.output_format == "csv":
+        writer = CSVWriter()
+    writer.write(test_info)
